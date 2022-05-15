007632893e038477c92bdfaa4383447691ea9c6d
AbdoNayer <abdelwahabnayer20@gmail.com>
2022-05-15 17:38:02 +0200

    done

--- js/script.js
+++ js/script.js
@@ -121,9 +121,3 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("resize", lazyload);
     window.addEventListener("orientationChange", lazyload);
 });
-
-addEventListener(document, "touchstart", function(e) {
-    console.log(e.defaultPrevented);  // will be false
-    e.preventDefault();   // does nothing since the listener is passive
-    console.log(e.defaultPrevented);  // still false
-}, Modernizr.passiveeventlisteners ? {passive: true} : false);
